7a7b64066d7476404fdcbd8c907aa83abee21cef
osama-hasan-khan <osamahasankhan098@gmail.com>
2023-07-27 12:27:59 +0530

    create header component and searchResult component and install react icons package

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,20 @@
 import React from "react";
-import { BrowserRouter } from "react-router-dom";
+
+// import browserRouter to navigate throut the page
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
+// importing all the components
 import Header from "./components/Header";
+import SearchResult from "./components/SearchResult ";
 
 const App = () => {
   return (
     <BrowserRouter>
       <div className="flex flex-col h-full">
         <Header />
+        <Routes>
+          <Route path="/searchResult/:searchQuery" element={<SearchResult />} />
+        </Routes>
       </div>
     </BrowserRouter>
   );
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,37 @@
-import React from 'react'
+import React, { useState } from "react";
+
+// react router packages
+import { Link, useLocation, useNavigate } from "react-router-dom";
+
+// images
+import ytLogo from "../images/yt-logo.png";
+import ytLogoMobile from "../images/yt-logo-mobile.png";
+
+// icons
+import { SlMenu } from "react-icons/sl";
+import { IoIosSearch } from "react-icons/io";
+import { RiVideoAddLine } from "react-icons/ri";
+import { FiBell } from "react-icons/fi";
+import { CgClose } from "react-icons/cg";
 
 const Header = () => {
-  return (
-    <div>Header</div>
-  )
-}
+  // state for tracking search string
+  const [searchQuery, setSearchQuery] = useState("");
 
-export default Header
+  // to navigate the search pages
+  const navigate = useNavigate();
+
+  // function for search on youtube
+  const searchQueryHandler = (event) => {
+    if (
+      (event?.key === "Enter" || event === "searchButton") &&
+      searchQuery.length > 0
+    ) {
+      navigate(`searchResult/${searchQuery}`);
+    }
+  };
+
+  return <div>Header</div>;
+};
+
+export default Header;

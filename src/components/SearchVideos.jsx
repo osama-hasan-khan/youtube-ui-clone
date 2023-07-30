f526004e2d6cb607a290c4e7a0ee234f7a9a1eed
osama-hasan-khan <osamahasankhan098@gmail.com>
2023-07-30 16:54:31 +0530

    created search video components

--- src/components/SearchVideos.jsx
+++ src/components/SearchVideos.jsx
@@ -1,12 +1,17 @@
 import React from "react";
+import { BiLike } from "react-icons/bi";
+import { AiOutlineDislike } from "react-icons/ai";
+import { TbShare3 } from "react-icons/tb";
+import { LiaDownloadSolid } from "react-icons/lia";
+import { BiDotsHorizontalRounded } from "react-icons/bi";
 
 const SearchVideos = ({ video, title, channelName }) => {
   return (
     <div className="">
-      <div className="flex flex-col">
+      <div className="flex flex-col w-[910px]">
         <img
           src={video}
-          className="w-[900px] h-[530px] object-contain cursor-pointer"
+          className="w-[910px] h-[530px] object-contain cursor-pointer"
         ></img>
         <span className="font-semibold">{title}</span>
         <div className="flex justify-between items-center">
@@ -23,6 +28,25 @@ const SearchVideos = ({ video, title, channelName }) => {
             <button className="p-2 px-6 text-white bg-black rounded-full font-semibold">
               Subscribe
             </button>
+          </div>
+          <div className="flex justify-between items-center cursor-pointer gap-2.5">
+            <div className="flex justify-around items-center rounded-full min-w-[130px] gap-3 bg-zinc-100 py-2 px-3">
+              <BiLike size={20} />
+              <span className="font-semibold">20k</span>
+              <span className="h-6 border-r border-r-zinc-300"></span>
+              <AiOutlineDislike size={20} />
+            </div>
+            <div className="flex justify-center items-center rounded-full gap-2 bg-zinc-100 py-2 px-3">
+              <TbShare3 size={20} />
+              <span className="font-semibold">Share</span>
+            </div>
+            <div className="flex justify-center items-center gap-2 rounded-full bg-zinc-100 py-2 px-3">
+              <LiaDownloadSolid />
+              <span className="font-semibold">Download</span>
+            </div>
+            <div className="flex items-center rounded-full bg-zinc-100 py-2 px-3">
+              <BiDotsHorizontalRounded size={20} />
+            </div>
           </div>
         </div>
       </div>
